48da8bfff2e5d915620a8d958bb602c43e9172c8
Joanderson Trajano <trajanojoanderson@gmail.com>
2023-01-19 19:19:28 -0300

    Primeiro Web Service

---
+++ src/main/java/com/estudo/projetospring/controllers/PrimeiroController.java
@@ -0,0 +1,16 @@
+package com.estudo.projetospring.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PrimeiroController {
+
+  @RequestMapping(method = RequestMethod.GET, path = "/ola")
+  public String ola() {
+    return "Óla spring";
+
+  }
+
+}
